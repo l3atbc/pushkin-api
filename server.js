865b1a22ab5a3a81a1b75191733c3a24decfcb54
Rob Wilkinson <wilkinson.robert.a@gmail.com>
2017-01-17 11:34:27 -0800

    api has /initalquestions and /trials

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const amqp = require('amqplib/callback_api');
+const bodyParser = require('body-parser')
+
 const app = require('express')();
 const PORT = 3000;
+app.use(bodyParser.json());
 
 amqp.connect(process.env.AMPQ_ADDRESS, function(err, conn) {
   if (err) {
@@ -25,6 +28,86 @@ amqp.connect(process.env.AMPQ_ADDRESS, function(err, conn) {
       res.end(`Sent ${msg} to queue ${channelName} `);
       ch.close();
     });
+  });
+  // Send a post with question id and response
+  // if none are received, assume initial request
+  app.get('/trials', (req, res, next) => {
+      var rpc = {
+        method: 'allTrials',
+      }
+      const channelName = 'db_worker';
+      conn.createChannel((err, ch) => {
+        if(err){
+          return next(err);
+        }
+        // create a unique queue
+        ch.assertQueue(null, {exclusive: true }, (err, q) => {
+          if(err) {
+            return next(err);
+          }
+          // generate a unique id to  listen for unique responses
+          var corr = generateUuid();
+          ch.consume(
+            q.queue,
+            msg => {
+              const content = JSON.parse(msg.content.toString('utf8'));
+              console.log('received', content)
+              if(msg.properties.correlationId === corr) {
+                // this is result of the RPC;
+                console.log('is a match')
+                res.json(content);
+                ch.close();
+              }
+            },
+            { noAck: true }
+          )
+          console.log('sent to queue', rpc.method, rpc.arguments);
+          ch.sendToQueue(channelName, new Buffer(JSON.stringify(rpc)), {
+            correlationId: corr,
+            replyTo: q.queue           
+          })
+        })
+      })
+
+  })
+  app.get('/initialQuestions', (req, res, next) => {
+      var rpc = {
+        method: 'getInitialQuestions',
+      }
+      const channelName = 'db_worker';
+      // create a channel
+      conn.createChannel((err, ch) => {
+        if(err){
+          return next(err);
+        }
+        // create a unique queue
+        ch.assertQueue(null, {exclusive: true }, (err, q) => {
+          if(err) {
+            return next(err);
+          }
+          // generate a unique id to  listen for unique responses
+          var corr = generateUuid();
+          ch.consume(
+            q.queue,
+            msg => {
+              const content = JSON.parse(msg.content.toString('utf8'));
+              console.log('received', content)
+              if(msg.properties.correlationId === corr) {
+                // this is result of the RPC;
+                console.log('is a match')
+                res.json(content);
+                ch.close();
+              }
+            },
+            { noAck: true }
+          )
+          console.log('sent to queue', rpc.method, rpc.arguments);
+          ch.sendToQueue(channelName, new Buffer(JSON.stringify(rpc)), {
+            correlationId: corr,
+            replyTo: q.queue           
+          })
+        })
+      })
   });
   app.get('/messages', (req, res) => {
     console.log('received request');
