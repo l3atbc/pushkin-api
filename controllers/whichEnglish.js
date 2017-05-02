8fdb681e28ba688bb5b33d784516bcde555f293b
Rob Wilkinson and Xiaolu Bai <icebunnydoll@gmail.com>
2017-05-01 17:35:27 -0700

    adds api/post/responses test to whichEnglishControllerTest

--- controllers/whichEnglish.js
+++ controllers/whichEnglish.js
@@ -1,11 +1,11 @@
-const express = require("express");
-function initialQuestions(rpc, conn) {
+const express = require('express');
+function initialQuestions(rpc, conn, dbWrite) {
   const router = new express.Router();
-  router.get("/api/initialQuestions", (req, res, next) => {
+  router.get('/api/initialQuestions', (req, res, next) => {
     var rpcInput = {
-      method: "getInitialQuestions"
+      method: 'getInitialQuestions'
     };
-    const channelName = "db_rpc_worker";
+    const channelName = 'db_rpc_worker';
     return rpc(conn, channelName, rpcInput)
       .then(data => {
         res.json(data);
@@ -13,19 +13,43 @@ function initialQuestions(rpc, conn) {
       .catch(next);
     // create a channel
   });
-  router.get("/api/responses", (req, res, next) => {
-    console.log("in route");
+  router.get('/api/responses', (req, res, next) => {
     const { user, choiceId, questionId } = req.body;
     var rpcInput = {
-      method: "allResponses",
+      method: 'allResponses',
       arguments: []
     };
-    const channelName = "db_rpc_worker";
+    const channelName = 'db_rpc_worker';
     return rpc(conn, channelName, rpcInput)
       .then(data => {
         res.json(data);
       })
       .catch(next);
+  });
+  router.post('/api/response', (req, res, next) => {
+    const { user, choiceId, questionId } = req.body;
+    // save in db
+    // ask for next
+    // respond
+    var rpcInput = {
+      method: 'createResponse',
+      arguments: [{ userId: user.id, choiceId }]
+    };
+    return dbWrite(conn, 'db_write', rpcInput)
+      .then(() => {
+        var workerInput = {
+          method: 'getQuestion',
+          payload: {
+            userId: user.id,
+            questionId,
+            choiceId
+          }
+        };
+        return rpc(conn, 'task_queue', workerInput);
+      })
+      .then(data => {
+        res.json(data);
+      });
   });
   return router;
 }
--- test/whichEnglishControllerTest.js
+++ test/whichEnglishControllerTest.js
@@ -1,10 +1,10 @@
 /*eslint-env mocha */
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const express = require("express");
-const request = require("supertest");
-const chai = require("chai");
-const sinon = require("sinon");
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const express = require('express');
+const request = require('supertest');
+const chai = require('chai');
+const sinon = require('sinon');
 
 const expect = chai.expect;
 
@@ -20,7 +20,12 @@ const expect = chai.expect;
 //   console.log("error!!!", err);
 //   res.json({ error: err.message });
 // });
-describe("WHICH English Controller", () => {
+const errorHandler = (err, req, res, next) => {
+  res.status(500);
+  console.log('error!!!', err);
+  res.json({ error: err.message });
+};
+describe('WHICH English Controller', () => {
   let app;
   beforeEach(() => {
     app = express();
@@ -28,38 +33,34 @@ describe("WHICH English Controller", () => {
     app.use(cors());
 
     app.use((req, resp, next) => {
-      req.test = "connect";
+      req.test = 'connect';
       next();
-    });
-    app.use((err, req, res, next) => {
-      res.status(500);
-      console.log("error!!!", err);
-      res.json({ error: err.message });
     });
   });
 
-  describe("GET /api/initialQuestions", function() {
-    it("respond with json", function() {
+  describe('GET /api/initialQuestions', function() {
+    it('respond with json', function() {
       const mockResponse = {
         questions: [{ id: 1 }, { id: 2 }]
       };
       let mockRpc = sinon.stub().returns(Promise.resolve(mockResponse));
 
-      const whichEnglishController = require("../controllers/whichEnglish")(
+      const whichEnglishController = require('../controllers/whichEnglish')(
         mockRpc,
-        "fake connection"
+        'fake connection'
       );
-      app.use("/", whichEnglishController);
+      app.use('/', whichEnglishController);
+      app.use(errorHandler);
       return request(app)
-        .get("/api/initialQuestions")
-        .set("Accept", "application/json")
+        .get('/api/initialQuestions')
+        .set('Accept', 'application/json')
         .expect(200)
-        .expect("Content-Type", /json/)
+        .expect('Content-Type', /json/)
         .then(response => {
-          const expectedChannel = "db_rpc_worker";
-          const connection = "fake connection";
+          const expectedChannel = 'db_rpc_worker';
+          const connection = 'fake connection';
           const body = {
-            method: "getInitialQuestions"
+            method: 'getInitialQuestions'
           };
           expect(mockRpc.getCalls()).to.have.length(1);
           const args = mockRpc.calledWith(connection, expectedChannel, body);
@@ -71,27 +72,29 @@ describe("WHICH English Controller", () => {
           expect(response.body).to.eql(mockResponse);
         });
     });
-    describe("GET /api/responses", () => {
-      it("should call rpc with allResponses", () => {
+    describe('GET /api/responses', () => {
+      it('should call rpc with allResponses', () => {
         const mockResponse = {
-          responses: ["response 1"]
+          responses: ['response 1']
         };
         let mockRpc = sinon.stub().returns(Promise.resolve(mockResponse));
-        const whichEnglishController = require("../controllers/whichEnglish")(
+        const whichEnglishController = require('../controllers/whichEnglish')(
           mockRpc,
-          "fake connection"
+          'fake connection'
         );
-        app.use("/", whichEnglishController);
+        app.use('/', whichEnglishController);
+        app.use(errorHandler);
+
         return request(app)
-          .get("/api/responses")
-          .set("Accept", "application/json")
+          .get('/api/responses')
+          .set('Accept', 'application/json')
           .expect(200)
-          .expect("Content-Type", /json/)
+          .expect('Content-Type', /json/)
           .then(response => {
-            const expectedChannel = "db_rpc_worker";
-            const connection = "fake connection";
+            const expectedChannel = 'db_rpc_worker';
+            const connection = 'fake connection';
             const body = {
-              method: "allResponses",
+              method: 'allResponses',
               arguments: []
             };
             const wasCalledWithArgs = mockRpc.calledWith(
@@ -101,10 +104,10 @@ describe("WHICH English Controller", () => {
             );
             const rpcArguments = mockRpc.getCall(0).args;
             expect(rpcArguments).to.have.length(3);
-            expect(rpcArguments[0]).to.eql("fake connection");
-            expect(rpcArguments[1]).to.eql("db_rpc_worker");
+            expect(rpcArguments[0]).to.eql('fake connection');
+            expect(rpcArguments[1]).to.eql('db_rpc_worker');
             expect(rpcArguments[2]).to.eql({
-              method: "allResponses",
+              method: 'allResponses',
               arguments: []
             });
             expect(wasCalledWithArgs).to.be.true;
@@ -114,8 +117,90 @@ describe("WHICH English Controller", () => {
             expect(response.body).to.eql(mockResponse);
           });
       });
-      // it("should return 200");
-      // it("should return json");
+    });
+    describe('POST /api/response', () => {
+      it('should insert workerInput to DB write with the user Id and the Choice ID', () => {
+        const mockResponse = {
+          responses: ['response 1']
+        };
+        let mockRpc = sinon.stub().returns(Promise.resolve(mockResponse));
+        let mockDbWrite = sinon.stub().returns(Promise.resolve());
+        const whichEnglishController = require('../controllers/whichEnglish')(
+          mockRpc,
+          'fake connection',
+          mockDbWrite
+        );
+        app.use('/', whichEnglishController);
+        app.use(errorHandler);
+
+        return request(app)
+          .post('/api/response')
+          .set('Content-Type', 'application/json')
+          .set('Accept', 'application/json')
+          .send({ user: { id: 1 }, questionId: 1, choiceId: 1 })
+          .expect(200)
+          .then(response => {
+            expect(mockDbWrite.called).to.be.true;
+            const dbWriterArguments = mockDbWrite.getCall(0).args;
+            expect(dbWriterArguments).to.have.length(3);
+            expect(dbWriterArguments[0]).to.eql('fake connection');
+            expect(dbWriterArguments[1]).to.eql('db_write');
+            expect(dbWriterArguments[2]).to.eql({
+              method: 'createResponse',
+              arguments: [{ userId: 1, choiceId: 1 }]
+            });
+            expect(
+              mockDbWrite.calledWith('fake connection', 'db_write', {
+                method: 'createResponse',
+                arguments: [{ userId: 1, choiceId: 1 }]
+              })
+            ).to.be.true;
+          });
+      });
+      it('should call rpc with createResponse and the passed in user, question and choice ID', () => {
+        const mockResponse = {
+          responses: ['response 1']
+        };
+        let mockRpc = sinon.stub().returns(Promise.resolve(mockResponse));
+        let mockDbWrite = sinon.stub().returns(Promise.resolve());
+        const whichEnglishController = require('../controllers/whichEnglish')(
+          mockRpc,
+          'fake connection',
+          mockDbWrite
+        );
+        app.use('/', whichEnglishController);
+        app.use(errorHandler);
+        return request(app)
+          .post('/api/response')
+          .set('Content-Type', 'application/json')
+          .set('Accept', 'application/json')
+          .send({ user: { id: 42 }, questionId: 100, choiceId: 1112 })
+          .expect(200)
+          .then(response => {
+            const expectedChannel = 'task_queue';
+            const connection = 'fake connection';
+            const body = {
+              method: 'getQuestion',
+              payload: { userId: 42, questionId: 100, choiceId: 1112 }
+            };
+            const wasCalledWithArgs = mockRpc.calledWith(
+              connection,
+              expectedChannel,
+              body
+            );
+            expect(mockDbWrite.called).to.be.true;
+            expect(mockRpc.called).to.be.true;
+            expect(mockRpc.getCall(0).args.length).to.equal(3);
+            expect(mockRpc.getCall(0).args[0]).to.equal('fake connection');
+            expect(mockRpc.getCall(0).args[1]).to.equal('task_queue');
+            expect(mockRpc.getCall(0).args[2]).to.eql({
+              method: 'getQuestion',
+              payload: { userId: 42, questionId: 100, choiceId: 1112 }
+            });
+
+            expect(wasCalledWithArgs).to.be.true;
+          });
+      });
     });
   });
 });
