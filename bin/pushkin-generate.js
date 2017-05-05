b4212a1493ac86b153266489aba7099bc40980d2
Rob Wilkinson and Xiaolu Bai <icebunnydoll@gmail.com>
2017-05-05 13:44:24 -0700

    moved general files to /bin

--- bin/pushkin-generate.js
+++ bin/pushkin-generate.js
@@ -7,9 +7,7 @@ const moment = require('moment');
 
 function addController(quizname) {
   try {
-    const from = path.resolve(
-      './controllers/generalController/generalController.js'
-    );
+    const from = path.resolve('./bin/generalController/generalController.js');
     const to = path.resolve(`./controllers/${quizname}.js`);
     fs.copy(from, to, (err, success) => {
       if (err) {
@@ -22,50 +20,103 @@ function addController(quizname) {
     console.log(chalk.red('please make sure to run this in a pushkin folder'));
   }
 }
-function addModel(quizname) {
-  try {
-    const schemas = fs.readdirSync(
-      path.resolve('../pushkin-db/migrations/generalSchemas')
-    );
-    const sortedSchemas = schemas.sort();
-    const mapObj = {
-      trials: `${quizname}_trials`,
-      questions: `${quizname}_questions`,
-      choices: `${quizname}_choices`,
-      users: `${quizname}_users`,
-      responses: `${quizname}_responses`
-    };
-    sortedSchemas.forEach((currentSchema, index) => {
-      return fs.readFile(
-        `../pushkin-db/migrations/generalSchemas/${currentSchema}`,
-        'utf8',
-        (err, data) => {
-          if (err) {
-            return console.log('err on reading file', err);
-          }
-          const re = new RegExp(Object.keys(mapObj).join('|'), 'gi');
-          const result = data.replace(re, matched => {
-            return mapObj[matched];
-          });
-          const formatedFileName = currentSchema.replace(/\d_/, '');
-          return fs.writeFile(
-            path.resolve(
-              `../pushkin-db/migrations/${moment()
-                .add(index, 'second')
-                .format(
-                  'YYYYMMDDHHmmss'
-                )}_create_${quizname}_${formatedFileName}`
-            ),
-            result,
-            function(err) {
-              if (err) return console.log('error while writing file', err);
-            }
-          );
+function checkIfFileExist(fileList, quizname) {
+  return fileList.some(element => {
+    return element.indexOf(quizname) > 0;
+  });
+}
+function copySchemas(quizname, mapObj, fileList) {
+  const schemas = fs.readdirSync(path.resolve('./bin/generalSchemas'));
+  const sortedSchemas = schemas.sort();
+  sortedSchemas.forEach((currentSchema, index) => {
+    return fs.readFile(
+      `../pushkin-api/bin/generalSchemas/${currentSchema}`,
+      'utf8',
+      (err, data) => {
+        if (err) {
+          return console.log('err on reading file', err);
         }
-      );
-    });
-  } catch (err) {
-    console.log('error!!', err);
+        const re = new RegExp(Object.keys(mapObj).join('|'), 'g');
+        const result = data.replace(re, matched => {
+          return mapObj[matched];
+        });
+        const formatedFileName = currentSchema.replace(/\d_/, '');
+        return fs.writeFile(
+          path.resolve(
+            `../pushkin-db/migrations/${moment()
+              .add(index, 'second')
+              .format('YYYYMMDDHHmmss')}_create_${quizname}_${formatedFileName}`
+          ),
+          result,
+          err => {
+            if (err) return console.log('error while writing file', err);
+          }
+        );
+      }
+    );
+  });
+}
+function copyModels(quizname, mapObj) {
+  fs.mkdirSync(`../pushkin-db/models/${quizname}`);
+  const models = fs.readdirSync(path.resolve('./bin/generalModels'));
+  models.forEach(currentModel => {
+    return fs.readFile(
+      `../pushkin-api/bin/generalModels/${currentModel}`,
+      'utf8',
+      (err, data) => {
+        if (err) {
+          return console.log('err on reading file', err);
+        }
+        const re = new RegExp(Object.keys(mapObj).join('|'), 'g');
+        const result = data.replace(re, matched => {
+          return mapObj[matched];
+        });
+        // const formatedFileName = currentModel.replace('.js', '');
+        return fs.writeFile(
+          path.resolve(
+            `../pushkin-db/models/${quizname}/${quizname}_${currentModel}`
+          ),
+          result,
+          err => {
+            if (err) return console.log('error while writing file', err);
+          }
+        );
+      }
+    );
+  });
+}
+function addModel(quizname) {
+  const schemaFileList = fs.readdirSync(
+    path.resolve('../pushkin-db/migrations')
+  );
+  const modelFileList = fs.readdirSync(path.resolve('../pushkin-db/models'));
+  if (
+    checkIfFileExist(schemaFileList, quizname) ||
+    checkIfFileExist(modelFileList, quizname)
+  ) {
+    return console.log(
+      chalk.red(
+        'quiz model already exist, please try editing the existing files'
+      )
+    );
+  } else {
+    try {
+      const mapObj = {
+        trials: `${quizname}_trials`,
+        questions: `${quizname}_questions`,
+        choices: `${quizname}_choices`,
+        users: `${quizname}_users`,
+        responses: `${quizname}_responses`
+      };
+      return new Promise((resolve, reject) => {
+        copySchemas(quizname, mapObj, schemaFileList);
+        resolve();
+      }).then(() => {
+        copyModels(quizname, mapObj);
+      });
+    } catch (err) {
+      console.log('error!!', err);
+    }
   }
 }
 program.parse(process.argv);
