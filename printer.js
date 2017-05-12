b3f720706d746a40fa5444c05b7e36aefaade528
Rob Wilkinson <wilkinson.robert.a@gmail.com>
2017-05-11 17:04:14 -0700

    add a small route printer

---
+++ printer.js
@@ -0,0 +1,78 @@
+'use strict';
+
+let _ = require('lodash');
+
+module.exports = (expressApp, filename) => {
+  const COLUMN_WIDTH_TREE = 50;
+  const COLUMN_WIDTH_PATH = 60;
+  const TREE_INDENT_SIZE = 5;
+
+  let text = [];
+
+  function fillWithSpaces(str, len) {
+    while (str.length < len) {
+      str += ' ';
+    }
+    return str;
+  }
+
+  function brushIndentation(indentation) {
+    return indentation.replace(/─/g, ' ').replace(/├/g, '│').replace(/└/g, ' ');
+  }
+
+  function printRoutes(layer, indentation) {
+    let path = ' ';
+    if (layer.path) {
+      path += layer.path;
+    } else if (layer.route && layer.route.path) {
+      path += layer.route.path;
+    } else if (layer.regexp) {
+      if (layer.regexp.source === '^\\/?$') {
+        path += '/';
+      } else if (layer.regexp.source === '^\\/?(?=\\/|$)') {
+        path += '*';
+      } else {
+        path += `/${layer.regexp.source}/`;
+      }
+    }
+
+    let methods = [];
+    if (layer.method) {
+      methods.push(layer.method);
+    } else if (layer.route) {
+      if (layer.route.methods) {
+        methods = _.keys(layer.route.methods);
+      } else if (layer.route.method) {
+        methods.push(layer.route.method);
+      }
+    }
+    methods = methods.join(', ').toUpperCase();
+
+    text.push(
+      `${fillWithSpaces(indentation + layer.name, COLUMN_WIDTH_TREE)}${fillWithSpaces(path, COLUMN_WIDTH_PATH)} ${methods}`
+    );
+
+    if (!layer.stack && !(layer.route && layer.route.stack)) {
+      if (layer.handle.stack) {
+        return printRoutes(layer.handle, brushIndentation(indentation));
+      }
+      return;
+    }
+
+    indentation = `${brushIndentation(indentation)} ├── `;
+
+    let stack = layer.stack || layer.route.stack;
+    for (let i = 0; i < stack.length; i += 1) {
+      if (i === stack.length - 1) {
+        indentation = `${indentation.substr(0, indentation.length - TREE_INDENT_SIZE)} └── `;
+      }
+      printRoutes(stack[i], indentation);
+    }
+
+    text.push(indentation.substr(0, indentation.length - TREE_INDENT_SIZE));
+  }
+
+  printRoutes(expressApp._router, '');
+
+  return text.join('\n');
+};
--- server.js
+++ server.js
@@ -7,11 +7,11 @@ const cors = require('cors');
 const fs = require('fs');
 const app = require('express')();
 const PORT = 3000;
+const printer = require('./printer');
 const path = require('path');
 
 app.use(bodyParser.json());
 app.use(cors());
-
 amqp.connect(process.env.AMPQ_ADDRESS, function(err, conn) {
   if (err) {
     return winston.error(err);
@@ -27,13 +27,6 @@ amqp.connect(process.env.AMPQ_ADDRESS, function(err, conn) {
     const controller = require('./controllers/' + short)(rpc, conn, dbWrite);
     app.use(route, controller);
   });
-  /*
-    Create and close a channel within the space of an http request
-    all channels are created on the same persistent rabbit mq connection
-    */
-
-  // Send a post with question id and choice id
-  // app
 
   app.post('/users', (req, res, next) => {
     const { user, choiceId, questionId } = req.body;
@@ -59,28 +52,20 @@ amqp.connect(process.env.AMPQ_ADDRESS, function(err, conn) {
       })
       .catch(next);
   });
+  app.get('/api/routes', (req, res, next) => {
+    const routes = printer(app);
+    res.send(routes);
+  });
   app.use((req, res, next) => {
     res.status(404).json({ message: 'Route not found' });
   });
-});
-app.get('/api/routes', (req, res, next) => {
-  res.json({
-    routes: app._router.stack.filter(stack => stack.route).map(stack => ({
-      methods: Object.keys(stack.route.methods).map(method =>
-        method.toUpperCase()
-      ),
-      path: stack.route.path
-    }))
+  app.use((err, req, res, next) => {
+    res.status(500);
+    res.json({ message: err.message });
+  });
+  app.listen(PORT, function() {
+    //Callback triggered when server is successfully listening. Hurray!
+    console.log('Server listening on: http://localhost:%s', PORT);
   });
 });
-app.use((err, req, res, next) => {
-  res.status(500);
-  res.json({ message: err.message });
-});
-
-app.listen(PORT, function() {
-  //Callback triggered when server is successfully listening. Hurray!
-  console.log('Server listening on: http://localhost:%s', PORT);
-});
-
 module.exports = app;
