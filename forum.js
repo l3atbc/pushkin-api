abcc7bfb3da6a207d002ecfee9c90a54157c438c
Xiaolu Bai <icebunnydoll@gmail.com>
2017-10-19 18:27:50 -0700

    api config file
    adds config.js to api
    
    removes comment in commit.js
    minor change in forum.js

---
+++ forum.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const channelName = 'forum_rpc_worker';
+
+module.exports = (rpc, conn, dbwrite) => {
+  const router = new express.Router();
+  router.get('/getAllForumPost', (req, res, next) => {
+    var rpcInput = {
+      method: 'allForumPost',
+      params: []
+    };
+    return rpc(conn, channelName, rpcInput)
+      .then(data => {
+        res.json(data);
+      })
+      .catch(next);
+  });
+
+  router.post('/createForumPost', (req, res, next) => {
+    var rpcInput = {
+      method: 'createForumPost',
+      params: [
+        {
+          auth0_id: req.body.auth0_id,
+          post_content: req.body.post_content,
+          stim_id: req.body.stim_id,
+          post_subject: req.body.post_subject,
+          created_at: req.body.created_at
+        }
+      ]
+    };
+    return rpc(conn, channelName, rpcInput)
+      .then(data => {
+        res.json(data);
+      })
+      .catch(next);
+  });
+  return router;
+};
